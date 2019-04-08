ab41263a4871f0e03b99c0a1058e050560df264f
lareniar <43707260+lareniar@users.noreply.github.com>
2019-04-08 12:21:06 +0200

    Add files via upload

--- Tienda_de_Musica/src/tienda_de_Musica/Articulos.java
+++ Tienda_de_Musica/src/tienda_de_Musica/Articulos.java
@@ -1,5 +1,23 @@
 package tienda_de_Musica;
 
 public class Articulos {
-
+	private int codArticulo;
+	private String nombre;
+	private String descripcion;
+	private int stock;
+	private int precio;
+	private boolean garantia;
+	private String categoria;
+	
+	
+	public articulos (int codArticulo, String nombre, String descripcion, int stock, int precio, boolean garantia, String categoria) {
+		this.codArticulo = codArticulo;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.stock = stock;
+		this.precio = precio;
+		this.garantia = garantia;
+		this.categoria = categoria;
+	}
+	
 }
